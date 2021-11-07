1e35f2f0bb6bb490399e783aed0dfba2ee297699
Georgetti, Stéfano <stefano.georgetti@accenture.com>
2021-11-07 10:43:34 -0300

    minor fx tweaks

--- data/actions/scripts/jakundaf desert/goldenHelmet/goldenHelmet.lua
+++ data/actions/scripts/jakundaf desert/goldenHelmet/goldenHelmet.lua
@@ -3,11 +3,11 @@ function onUse(cid, item, pos)
 	fail = {x = 32672, y = 32137, z = 7, stackpos = 255}
 	if item.uid == 64699 and getPlayerItemCount(cid, 2523) >= 1 or getPlayerItemCount(cid, 2471) >= 1 then
 		doTeleportThing(cid, entrance)
-		doSendMagicEffect(entrance, 12)
+		doSendMagicEffect(entrance, 2)
 		doPlayerSendTextMessage(cid, MESSAGE_EVENT_ADVANCE, "The entity inside the lamp whispers says that you cannot leave the island with the gods equipment.")
 	else
 		doTeleportThing(cid, fail)
-		doSendMagicEffect(fail, 2)
+		doSendMagicEffect(fail, 12)
 	end
 
 	return true
